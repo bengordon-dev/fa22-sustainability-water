d088c7621d709b5d7bcffa042379a7d1e1d6a75d
Ben Gordon <benjamintheepic@gmail.com>
2022-11-09 17:59:49 -0600

    first pass of renewable energy graph

--- Launder/pages/Graph.js
+++ Launder/pages/Graph.js
@@ -1,68 +1,131 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, Button } from 'react-native';
+import { StyleSheet, Text, View, ScrollView, Button } from 'react-native';
 import { useState, useEffect } from 'react';
 import Svg, { Rect, Circle, Line } from 'react-native-svg';
+import { Text as SVGText }  from 'react-native-svg';
 
 
 export default function Graph(props) {
   const [graphData, setGraphData] = useState({})
+  const [renewData, setRenewData] = useState({})
   const [region, setRegion] = useState("lzLcra")
-  const [day, setDay] = useState("damSppData"); // damSppData or rtSppData
+  const [day, setDay] = useState("currentDay"); // damSppData or rtSppData
   const [points, setPoints] = useState([])
   const [maxPrice, setMaxPrice] = useState(1)
+  const [renewPoints, setRenewPoints] = useState([])
+  const [maxRenewProd, setMaxRenewProd] = useState(1)
   
   useEffect(() => {
     fetch("http://127.0.0.1:5000/getSystemWidePrices").then((res) => res.json())
     .then((res) => {
       setGraphData(res)
     })
+    fetch("http://127.0.0.1:5000/getCombinedWindandSolar").then((res) => res.json())
+    .then((res) => setRenewData(res))
   }, [])
 
   // change the points after the graph data gets changed 
   useEffect(() => {
-    graphData[day] && setPoints(graphData[day].map(e => ({
-      timeStamp: e.interval,
-       price: e[region]
-      })))
-  }, [graphData, day])
+    if (graphData.rtSppData && graphData.damSppData) {
+      let newRTPoints = [...graphData.rtSppData.map(e => ({
+        timeStamp: e.interval,
+        price: e[region]
+      }))]
+      let maxTimeStamp = Math.max(...newRTPoints.map(e => e.timeStamp))
+      let newDAMPoints = [...graphData.damSppData.map(e => ({
+          timeStamp: e.interval,
+          price: e[region]
+        })).filter(e => e.timeStamp > maxTimeStamp)
+      ]
+      setPoints([...newRTPoints, ...newDAMPoints])
+    }
+  }, [graphData])
 
   useEffect(() => {
-    graphData && graphData.damSppData && graphData.rtSppData && 
-      setMaxPrice(Math.max(...graphData.damSppData.map(e => e[region]), ...graphData.rtSppData.map(e => e[region])))
-  }, [graphData])
+
+    if (renewData[day]) {
+      let newRenewData = []
+      if (day === "currentDay") {
+        for (const hrEntry of Object.entries(renewData[day].data)) {
+          const hourEntry = hrEntry[1]
+          //console.log(hourEntry)
+          let entry = {hour: hourEntry.hourEnding}
+          if (hourEntry.actualWind && hourEntry.actualSolar) {
+            entry.combined = hourEntry.actualWind + hourEntry.actualSolar
+          } else {
+            entry.combined = hourEntry.copHslWind + hourEntry.copHslSolar
+          }
+          newRenewData.push(entry)
+        } 
+      } else {
+        for (const hrEntry of Object.entries(renewData[day].data)) {
+          const hourEntry = hrEntry[1]
+          let entry = {hour: hourEntry.hourEnding}
+          entry.combined = hourEntry.copHslSolarDayAhead + hourEntry.copHslWindDayAhead
+          newRenewData.push(entry)
+        }  
+      }
+      console.log(newRenewData)
+      setRenewPoints(newRenewData)
+      setMaxRenewProd(Math.max(...newRenewData.map(e => e.combined)))
+    } 
+  }, [renewData, day])
+
+  useEffect(() => { 
+    points.length > 0 && setMaxPrice(Math.max(...points.map(e => e.price)))
+  }, [points])
   
   return (
-    <View style={styles.container}>
+    <ScrollView contentContainerStyle={styles.container}>
       <Button
         onPress={props.goHome}
         title="Go home"
         color="#841584"
       />
-      <Button title={day === "damSppData" ? "Day Ahead data" : "Real-time data"}
-        onPress={() => setDay(day == "damSppData" ? "rtSppData" : "damSppData")}/>
-      <View width="100%" alignItems="center" justifyContent="center">
-        <Svg height="50%" width="80%" viewBox="0 0 150 100" >
-          <Line x1="0" y1="100" x2="150" y2="100" stroke="black" strokeWidth="2" />
-          <Line x1="1" y1="0" x2="1" y2="100" stroke="black" strokeWidth="2" />
+      <View width="100%" height={400} alignItems="center" justifyContent="center">
+        <Svg height="100%" width="90%" viewBox="0 0 170 120" >
+          <Line x1="20" y1="100" x2="170" y2="100" stroke="black" strokeWidth="2" />
+          <Line x1="21" y1="0" x2="21" y2="100" stroke="black" strokeWidth="2" />
           {points.length > 0 && points.map((point, i) => {
               const now = new Date(point.timeStamp)
               const hoursElapsed = (point.timeStamp - (point.timeStamp - 1000*(now.getHours()*3600 + now.getMinutes()*60 + now.getSeconds() ))) / 3600000 
               //console.log(hoursElapsed)
-
-              return (<Circle cx={ /*(point.timeStamp-firstTime)/(lastTime-firstTime)*/hoursElapsed/24*150} 
+              return (<Circle cx={20 + /*(point.timeStamp-firstTime)/(lastTime-firstTime)*/hoursElapsed/24*150} 
                       cy={100-(point.price)/(maxPrice)*100} 
-                      r={1} key={i} fill="red">
+                      r={.5} key={i} fill="red">
                       </Circle>) 
             }
           )}
+          {[...Array(13).keys()].map(e => <SVGText x={20 + e/13*150 + 150/48} y={110} fill="black" fontSize="5">{e*2}</SVGText>)}
+          <SVGText x={10} y={100} fill="black" fontSize="5">{0}</SVGText>
+          <SVGText x={5} y={5} fill="black" fontSize="5">{maxPrice}</SVGText>
         </Svg>
-        <View width="100%" height={50} flexDirection="row" justifyContent="center">{[...Array(4).keys()].map(e => <Text marginLeft={50} color="black">{e}</Text>)}</View>
       </View>
       <Text>High: {Math.max(...points.map(e => e.price))}</Text>
       <Text>Low: {Math.min(...points.map(e => e.price))}</Text>
       <Text>First: {points.length > 0  && (new Date(points[0].timeStamp)).toTimeString()}</Text>
       <Text>Last: {points.length > 0  && (new Date(points[points.length-1].timeStamp - 1000)).toTimeString()}</Text>
-    </View>
+      <Button title={day === "currentDay" ? "Current Day" : "Next Day"}
+        onPress={() => setDay(day == "currentDay" ? "nextDay" : "currentDay")}/>
+
+      <View width="100%" height={400} alignItems="center" justifyContent="center">
+        <Svg height="100%" width="90%" viewBox="0 0 170 120" >
+          <Line x1="20" y1="100" x2="170" y2="100" stroke="black" strokeWidth="2" />
+          <Line x1="21" y1="0" x2="21" y2="100" stroke="black" strokeWidth="2" />
+          {renewPoints.length > 0 && renewPoints.map((point, i) => {
+              //console.log(hoursElapsed)
+              return (<Circle cx={20 + point.hour/24*150} 
+                      cy={100-(point.combined)/(maxRenewProd)*100} 
+                      r={.5} key={i} fill="red">
+                      </Circle>) 
+            }
+          )}
+          {[...Array(13).keys()].map(e => <SVGText x={20 + e/13*150 + 150/48} y={110} fill="black" fontSize="5">{e*2}</SVGText>)}
+          <SVGText x={10} y={100} fill="black" fontSize="5">{0}</SVGText>
+          <SVGText x={5} y={5} fill="black" fontSize="5">{maxRenewProd}</SVGText>
+        </Svg>
+      </View>
+    </ScrollView>
   );
 }
 
@@ -72,5 +135,6 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+    paddingTop: 100,
   },
 });
